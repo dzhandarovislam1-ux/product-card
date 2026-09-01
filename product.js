08039d291969becc3e20eeaa1864ba26684feb1f
dzhandarovislam1-ux <dzhandarov.islam1@gmail.com>
2026-09-01 12:50:00 +0300

    (Homework №10):Work at JS

---
+++ product.js
@@ -0,0 +1,62 @@
+const products = [
+  {
+    image: "assets/moisturizing-mousse.jpg",
+    category: "Для нормальной кожи",
+    name: "Увлажняющий мусс",
+    description: "Глубоко увлажняют кожу лица,оставляя её мягкой и гладкой",
+    compound: [
+      "активные натуральные комплексы",
+      "витамины С, А, РР, В И Е",
+      "солнцезащитные компоненты",
+    ],
+    price: "2 750 &#8381",
+    buttonColor: "",
+  },
+
+  {
+    image: "assets/moisturizing-mask.jpg",
+    category: "Для нормальной кожи",
+    name: "Увлажняющая маска",
+    description: "Способствует удерживанию влаги в верхних слоях кожи",
+    compound: ["воски", "минералы", "масла"],
+    price: "3 500 &#8381",
+    buttonColor: "button-blue",
+  },
+
+  {
+    image: "assets/cleansing-gel.jpg",
+    category: "Для нормальной кожи",
+    name: "Гель для умывания",
+    description: "Интенсивно очищает,не повреждает защитный барьер кожи",
+    compound: [
+      "минералы",
+      "витамины C,A,PP,B И E",
+      "солнцезащитные компоненты",
+    ],
+    price: "1 650 &#8381",
+    buttonColor: "button-green",
+  },
+
+  {
+    image: "assets/gift-set-number-one.jpg",
+    category: "Для нормальной кожи",
+    name: "Подарочный набор №1",
+    description: "Набор, состоящий из увлажняющего крема и маски",
+    compound: ["воски", "минералы", "масла"],
+    price: "4 750 &#8381",
+    buttonColor: "button-red",
+  },
+
+  {
+    image: "assets/gift-set-number-five.jpg",
+    category: "Для нормальной кожи",
+    name: "Подарочный набор №5",
+    description:
+      "Весь набор средств Invisible symphony, крем, маска, мусс и гель для умывания",
+    compound: ["воски", "минералы", "масла"],
+    price: "7 520 &#8381",
+    buttonColor: "button-black",
+  },
+];
+
+export default products;
